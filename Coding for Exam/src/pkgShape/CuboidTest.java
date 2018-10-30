e8bb548e7f2b392e48b9c3cc840da14e93c14185
kojoaddo <kojoaddo@wifi-roaming-128-4-86-84.host.udel.edu>
2018-10-30 13:32:56 -0400

    commit

--- Coding for Exam/src/pkgShape/CuboidTest.java
+++ Coding for Exam/src/pkgShape/CuboidTest.java
@@ -1,5 +1,67 @@
 package pkgShape;
 
-public class CuboidTest {
+import pkgShape.Cuboid.SortByArea;
+import pkgShape.Cuboid.SortByVolume;
 
+class CuboidTest {
+	@Test
+	public void constructorTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		assertEquals(true, myCube instanceof Cuboid);
+	}
+	@Test
+	public void getDepthTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		assertEquals(myCube.getDepth(),4);
+	}
+	@Test
+	public void setDepthTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		myCube.setDepth(8);
+		assertEquals(myCube.getDepth(), 8);
+	}
+	@Test
+	public void volumeTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		assertEquals(myCube.volume(),24);
+	}
+	@Test
+	public void areaTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		assertEquals(myCube.area(), 52);
+	}
+	@Test
+	public void perimeterThrowsUnsupportedOperationExceptionTest() {
+		try{
+			Cuboid myCube = new Cuboid(2,3,4);
+			myCube.perimeter();
+		}
+		catch(UnsupportedOperationException e) {
+			
+		}
+	}
+	@Test 
+	public void sortByAreaTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		Cuboid myOtherCube = new Cuboid(4,5,6);
+		SortByArea mySort = myCube.new SortByArea();
+		
+		assertTrue(mySort.compare(myCube, myOtherCube) > 0);
+		assertTrue(mySort.compare(myOtherCube, myCube) < 0);
+		assertTrue(mySort.compare(myCube, myCube) == 0);
+	}
+	@Test
+	public void sortByVolumeTest() {
+		Cuboid myCube = new Cuboid(2,3,4);
+		Cuboid myOtherCube = new Cuboid(4,5,6);
+		SortByVolume mySort = myCube.new SortByVolume();
+		
+		assertTrue(mySort.compare(myCube, myOtherCube) < 0);
+		assertTrue(mySort.compare(myOtherCube, myCube) > 0);
+		assertTrue(mySort.compare(myCube, myCube) == 0);
+	}
+	private void assertTrue(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- Coding for Exam/src/pkgShape/RectangleTest.java
+++ Coding for Exam/src/pkgShape/RectangleTest.java
@@ -1,5 +1,52 @@
 package pkgShape;
 
-public class RectangleTest {
+import org.junit.Test;
 
+class RectangleTest {
+
+	@Test
+	public void constructorTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		assertEquals(true, myRectangle instanceof Rectangle);
+	}
+	@Test
+	public void getLengthTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		assertEquals(3,myRectangle.getLength());
+	}
+	@Test
+	public void getWidthTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		assertEquals(2,myRectangle.getWidth());
+	}
+	@Test
+	public void setLengthTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		myRectangle.setLength(5);
+		assertTrue(5 == myRectangle.getLength());
+	}
+	@Test
+	public void setWidthTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		myRectangle.setWidth(7);
+		assertTrue(7 == myRectangle.getWidth());
+	}
+	@Test
+	public void areaTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		assertEquals(6, myRectangle.area());
+	}
+	@Test
+	public void perimeterTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		assertEquals(10, myRectangle.perimeter());
+	}
+	@Test
+	public void compareToTest() {
+		Rectangle myRectangle = new Rectangle(2,3);
+		Rectangle mySecondRectangle = new Rectangle(7,3);
+		assertTrue(myRectangle.compareTo(mySecondRectangle) < 0);
+		assertTrue(mySecondRectangle.compareTo(myRectangle) > 0);
+		assertTrue(myRectangle.compareTo(myRectangle) == 0);
+	}
 }
